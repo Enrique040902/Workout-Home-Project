7d1a744116b86366e15232647899d788ac3d5fa1
Enrique Sánchez <dilanenrique555@gmail.com>
2024-04-22 20:09:25 -0600

    Se eliminaron los imports inecesarios

--- app/src/main/java/com/example/proyectofis/PrincipalActivity.kt
+++ app/src/main/java/com/example/proyectofis/PrincipalActivity.kt
@@ -3,14 +3,9 @@ package com.example.proyectofis
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.proyectofis.databinding.ActivityPrincipalBinding
-import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.SetOptions
 
