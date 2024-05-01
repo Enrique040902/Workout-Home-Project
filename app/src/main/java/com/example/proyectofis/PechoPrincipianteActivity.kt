df066e402cd375b7d4c0e7e05cac22e75fd5cc6f
Enrique Sánchez <dilanenrique555@gmail.com>
2024-05-01 15:17:27 -0600

    Implementación de la nueva UI de Pecho Principiante

--- app/src/main/java/com/example/proyectofis/PechoPrincipianteActivity.kt
+++ app/src/main/java/com/example/proyectofis/PechoPrincipianteActivity.kt
@@ -1,44 +1,43 @@
 package com.example.proyectofis
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.proyectofis.databinding.ActivityPechoPrincipianteBinding
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.proyectofis.screens.pecho.PechoPrincipianteComponent
+import com.example.proyectofis.ui.theme.PROYECTOFISTheme
 
-class PechoPrincipianteActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityPechoPrincipianteBinding
-
+class PechoPrincipianteActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityPechoPrincipianteBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.saltosTijeraRutina.setContent { RutinaComponent("30 seg") }
-        binding.FlexionesInclinacion.setContent { RutinaComponent(rutina = "15 reps") }
-        binding.FlexionesNormales.setContent { RutinaComponent(rutina = "15 reps") }
-        binding.FlexionesAbiertas.setContent { RutinaComponent(rutina = "10 reps") }
-        binding.FlexionesTricepSilla.setContent { RutinaComponent(rutina = "10 reps") }
-        binding.FlexionesInclinacion2.setContent { RutinaComponent(rutina = "10 reps") }
-        binding.FlexionesAbiertas2.setContent { RutinaComponent(rutina = "10 reps") }
-        binding.FlexionesTricepSilla2.setContent { RutinaComponent(rutina = "10 reps") }
-        binding.FlexionesApoyoRodilla.setContent { RutinaComponent(rutina = "15 reps") }
+        setContent {
+            PROYECTOFISTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PechoPrincipianteComponent()
+                }
+            }
+        }
 
     }
 
+    @Preview
     @Composable
-    private fun RutinaComponent(rutina: String) {
-        Text(
-            modifier = Modifier.padding(3.dp),
-            text = rutina,
-            fontSize = 14.sp,
-            fontWeight = FontWeight(600)
-        )
+    private fun PechoPrincipianteActivityPreview() {
+        PROYECTOFISTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                PechoPrincipianteComponent()
+            }
+        }
     }
 }
